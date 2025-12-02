17093f99a4a492db34e211427d009297de46a7d5
AnrangoC <cdanrangom@utn.edu.ec>
2025-12-02 16:41:28 -0500

    Change Ronda class to public and add properties

--- ClasesTorneo/Ronda.cs
+++ ClasesTorneo/Ronda.cs
@@ -1,12 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClasesTorneo
 {
-    internal class Ronda
+    public class Ronda
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int TorneoId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        public int Orden { get; set; }
+
+        [Required]
+        public string TipoFase { get; set; } = string.Empty;
+
+        public bool EstaCompleta { get; set; }
+
+        public Torneo? Torneo { get; set; }
+        public List<Partido>? Partidos { get; set; }
     }
 }
