06b7746b9a511e1d11ed465f7d52c45732444528
AnrangoC <cdanrangom@utn.edu.ec>
2025-12-02 16:40:31 -0500

    Update Jugador.cs

--- ClasesTorneo/Jugador.cs
+++ ClasesTorneo/Jugador.cs
@@ -1,16 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClasesTorneo
 {
-    internal class Jugador
+    public class Jugador
     {
-        public int id;
-        public Boolean nombre;
-        public Equipo equipo;
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Nombre { get; set; } = string.Empty; 
 
+        public int EquipoId { get; set; }
+        
+        public bool EstaSancionado { get; set; } 
+
+        public Equipo? Equipo { get; set; }
+        public List<EstadisticaPartido>? Estadisticas { get; set; }
+        public List<Sancion>? Sanciones { get; set; }
     }
 }
