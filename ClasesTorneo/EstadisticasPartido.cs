a71dd5372a9cce8711e263498fcfc7ac2d3e6e4a
AnrangoC <cdanrangom@utn.edu.ec>
2025-12-02 16:40:04 -0500

    Update EstadisticasPartido.cs

--- ClasesTorneo/EstadisticasPartido.cs
+++ ClasesTorneo/EstadisticasPartido.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClasesTorneo
 {
-    internal class EstadisticasPartido
+    public class EstadisticaPartido
     {
+        [Key]
+        [Required]
+        public string TipoEvento { get; set; } = string.Empty;
+        public int? Minuto { get; set; }
+
+        public Partido? Partido { get; set; }
+        public Jugador? Jugador { get; set; }
     }
 }
