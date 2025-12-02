3ce4ef94b359d2817ed8654c0407b2766b0c5bb9
AnrangoC <cdanrangom@utn.edu.ec>
2025-12-02 16:41:47 -0500

    Update Sancion.cs

--- ClasesTorneo/Sancion.cs
+++ ClasesTorneo/Sancion.cs
@@ -1,18 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClasesTorneo
 {
-    internal class Sancion
+    public class Sancion
     {
-        public int id;
-        public Jugador jugador;
-        public Partido partido;
-        public string tipo; // Ejemplo: "Tarjeta Amarilla", "Tarjeta Roja"
-        public string descripcion;
-        public string Estado;
+        [Key]
+        public int Id { get; set; }
+
+        public int JugadorId { get; set; }
+        public int PartidoOrigenId { get; set; }
+        public int PartidoSuspendidoId { get; set; }
+
+        public bool Cumplida { get; set; }
+
+        public Jugador? Jugador { get; set; }
+        public Partido? PartidoOrigen { get; set; }
+        public Partido? PartidoSuspendido { get; set; }
     }
 }
