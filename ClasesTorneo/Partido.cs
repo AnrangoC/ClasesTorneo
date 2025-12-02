7949f5d0d5406604155fa7016db3180d57f57077
AnrangoC <cdanrangom@utn.edu.ec>
2025-12-02 16:40:57 -0500

    Update Partido.cs

--- ClasesTorneo/Partido.cs
+++ ClasesTorneo/Partido.cs
@@ -1,16 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClasesTorneo
 {
-    internal class Partido
+    public class Partido
     {
-        public int id;
-        public Ronda ronda;
-        public EstadisticasPartido estadisticas;
-        public List<Sancion> sanciones;
+        [Key]
+        public int Id { get; set; }
+
+        public int TorneoId { get; set; }
+        public int RondaId { get; set; }
+        public int EquipoLocalId { get; set; }
+        public int EquipoVisitanteId { get; set; }
+
+        public DateTime FechaHora { get; set; }
+
+        public bool Jugado { get; set; }
+
+        public int GolesLocal { get; set; }
+        public int GolesVisitante { get; set; }
+
+        public int? GanadorId { get; set; }
+
+        public Torneo? Torneo { get; set; }
+        public Ronda? Ronda { get; set; }
+        public Equipo? EquipoLocal { get; set; }
+        public Equipo? EquipoVisitante { get; set; }
+        public Equipo? Ganador { get; set; }
+        public List<EstadisticaPartido>? Estadisticas { get; set; }
     }
 }
